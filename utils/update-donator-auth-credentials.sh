4b1b6e040d8bbb5889480f07dc743ddf21acfd21
Marc Hermans <marc.hermans@ldtteam.com>
2025-12-13 15:24:35 +0100

    Update secrets

--- utils/update-donator-auth-credentials.sh
+++ utils/update-donator-auth-credentials.sh
@@ -13,5 +13,5 @@ read -s REFRESH_TOKEN
 
 echo
 
-./update-secret-template.sh "ldtteam" "donator-auth" "donator-auth-ldtteam-authentication-server" "Patreon.ApiClientSecret" "$CLIENT_SECRET"
-./update-secret-template.sh "ldtteam" "donator-auth" "donator-auth-ldtteam-authentication-server" "Patreon.InitializingApiRefreshToken" "$REFRESH_TOKEN"
+./update-secret-template.sh "ldtteam" "donator-auth-ha" "donator-auth-ldtteam-authentication-server" "Patreon.ApiClientSecret" "$CLIENT_SECRET"
+./update-secret-template.sh "ldtteam" "donator-auth-ha" "donator-auth-ldtteam-authentication-server" "Patreon.InitializingApiRefreshToken" "$REFRESH_TOKEN"
